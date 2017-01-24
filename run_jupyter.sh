0ef85abec9782e34d5a5bf699e11e5642931436f
AtchiReddy <atchireddi@gmail.com>
2017-01-24 05:38:45 +0000

    Final Submission

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,2 +1,2 @@
-jupyter nbconvert --execute --ExecutePreprocessor.timeout=-1 --to notebook --inplace Traffic_Sign_Classifier_DropOut-3xConv1Filters-FC1024.ipynb
+jupyter nbconvert --execute --ExecutePreprocessor.timeout=-1 --to notebook --inplace Traffic_Sign_Classifier_Submission.ipynb
 echo "Done 1024" | mail -s "Done 1024" -u atchirc@marvell.com
